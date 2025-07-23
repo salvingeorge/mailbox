ab6bc4a7783e57846b75973ba487becef6bcef24
salvin.george <salvin.george@fkie.fraunhofer.de>
2025-07-23 13:35:57 +0200

    deployment 1

--- galactic-postbox/src/services/api.js
+++ galactic-postbox/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://postbox-backend.onrender.com/api';
 
 const apiService = {
   // Get token from localStorage
